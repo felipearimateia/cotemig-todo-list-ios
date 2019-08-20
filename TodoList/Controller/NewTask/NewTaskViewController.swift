acde704457481800237057d3cf3d0027fc99e675
Felipe Arimateia <felipearimateia@gmail.com>
2019-08-20 18:33:01 -0300

    update background

--- TodoList/Controller/NewTask/NewTaskViewController.swift
+++ TodoList/Controller/NewTask/NewTaskViewController.swift
@@ -13,7 +13,7 @@ class NewTaskViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .clear
+//        view.backgroundColor = .clear
         view.isOpaque = true
     }
     
